8dd54ff3cc4e3e4804d790849cff5781a38013c1
Kenneth Kufluk <kenneth@kufluk.com>
2011-03-25 16:12:05 -0700

    work in progress

--- test/modules/modc.js
+++ test/modules/modc.js
@@ -1,9 +1,25 @@
 provide(function(exports) {
-  using('moda', function(moda) {
+
+  using('moda', 'modab', 'javascripts/d.js', '$mode.js', 'modb', function(moda, ab, b) {
+
+    using('mode', function(e) {
+    });
+    using('moda', function(a) {
+      exports({
+        test: function() {
+          return moda.test + ' from mod a';
+        }
+      });
+    });
+  });
+
+  using('moda', function(a) {
     exports({
       test: function() {
         return moda.test + ' from mod a';
       }
     });
   });
+
+
 });
