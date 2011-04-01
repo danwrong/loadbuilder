1dd41cad558c4fe6e632cd34ad8ceace9d77e7b8
Kenneth Kufluk <kenneth@kufluk.com>
2011-04-01 15:40:44 -0700

    bundling running from build_bundle script

---
+++ build_bundle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var loadbuilder = require('loadbuilder');
+
+var phoenixBuilder = loadbuilder.builder({
+  matchers: [],
+  nomin: true,
+  follow: true,
+  docRoot: 'test',
+  modPath: '/modules',
+  distRoot: 'test/dist'
+});
+
+var cbundle = phoenixBuilder.bundle('modc');
+
+var dbundle = phoenixBuilder.bundle('modd', {
+  exclude: [cbundle]
+});
+
+phoenixBuilder.bundle('modb', {
+  exclude: ['moda', cbundle, dbundle]
+});
+
+
+// ==> output inline script manifest
