76e523c063b4681c423de67df48dbf75a4cc88f1
Kenneth Kufluk <kenneth@kufluk.com>
2011-04-06 17:57:10 -0700

    Doh.  Fixed mising provides.

--- test/modules/mod4-script.js
+++ test/modules/mod4-script.js
@@ -1,4 +1,4 @@
-provide(function() {
+provide(function(exports) {
   using('javascripts/script1.js', function() {  // S1 provides global S1 var
     using('mod4_a', function(a) { // A needs S1
       using('$../javascripts/script2.js', function() {  // S2 uses S1, provides global S2 var
