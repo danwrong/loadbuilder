d43227617b5fcc9288fa66976cc51827a3782808
Kenneth Kufluk <kenneth@twitter.com>
2012-03-08 17:33:05 -0800

    Remove node_modules from npm to keep with the fashions.

---
+++ node_modules/uglify-js/test/unit/compress/expected/except.js
@@ -0,0 +1,1 @@
+(function(){function j(){i=2;var h=3,a=3}var a=1,b=1,c=1,d=1,e=1,f=1,g=1,h=1,i=1;j()})()
---
+++ node_modules/uglify-js/test/unit/compress/test/except.js
@@ -0,0 +1,17 @@
+(function() {
+  var something_really_long=1;
+  var b1=1;
+  var c1=1;
+  var d1=1;
+  var e1=1;
+  var f1=1;
+  var g1=1;
+  var h=1;
+  var h1=1;
+  function test() {
+    h1=2;
+    var h=3;
+    var g=3;
+  }
+  test();
+}());
